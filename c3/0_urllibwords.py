68da8aba786dabfdef5973dcc8c8046b3379debd
Ricoistmeinname <53438525+Ricoistmeinname@users.noreply.github.com>
2020-03-04 17:40:21 +0800

    Upload - 20200304

---
+++ c3/0_urllibwords.py
@@ -0,0 +1,11 @@
+# Access web pages with urllib, and play with it
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+counts = dict()
+for line in fhand:
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+print(counts)
