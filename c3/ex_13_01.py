625a7ccb70e11b3481f24d9a62e01043a896a104
Ricoistmeinname <53438525+Ricoistmeinname@users.noreply.github.com>
2020-03-05 17:50:24 +0800

    Upload - 20200305

---
+++ c3/ex_13_01.py
@@ -0,0 +1,43 @@
+# Exercise 13.1 - Extracting Data from XML
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter location:')
+if len(url) < 1: url = 'http://py4e-data.dr-chuck.net/comments_309956.xml'
+print('Retrieving', url)
+uhand = urllib.request.urlopen(url, context=ctx)
+
+data = uhand.read()
+print('Retrieved', len(data), 'characters')
+#print(data.decode())
+tree = ET.fromstring(data)
+
+# Retrieve all the tag 'count'
+counts = tree.findall('./comments/comment/count')
+print('Count:', len(counts))
+
+# Retrieve all of the text under the tag
+lst = list()
+i = 0
+for count in counts:
+    snum = counts[i].text
+    lst.append(int(snum))
+    i = i + 1
+#print(lst)
+# Sum up all the value in the list
+print('Sum:', sum(lst))    
+
+# Convert all the string to integer with in the list and sum it up   
+#print(sum(int(num) for num in lst))
+
+
+
+
+# http://py4e-data.dr-chuck.net/comments_42.xml
+# http://py4e-data.dr-chuck.net/comments_309956.xml
